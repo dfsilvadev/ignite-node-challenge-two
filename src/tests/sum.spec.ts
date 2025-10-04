25424577e49d5b6fe6228977f0d8e58e60a00dd0
Daniel Silva <dfsilva.dxp@gmail.com>
2025-10-04 11:03:49 -0300

    test: add test

--- src/tests/sum.spec.ts
+++ src/tests/sum.spec.ts
@@ -4,4 +4,8 @@ describe("Sum", () => {
   it("should add two positive number correctly", () => {
     expect(sum(1, 3)).toBe(4);
   });
+
+  it("should receive '5, 5' and return 10", () => {
+    expect(sum(5, 5)).toBe(10);
+  });
 });
